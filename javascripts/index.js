6389180b4b09da68bc2669f564ef021b6662efb4
ae95medina <ae95medina@gmail.com>
2022-06-18 17:13:34 -0500

    submit default prevented

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,4 +1,11 @@
 console.log("hi")
+document.addEventListener('DOMContentLoaded', () => {
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault()
+})
+
+})
+
 function addWorkouts(){
 // const createWorkout = document.querySelector("submit")
 // createWorkout.addEventListener("submit", event){
@@ -6,9 +13,8 @@ function addWorkouts(){
 // }
 fetch("http://localhost:3000/workouts")
     .then (response => response.json())
-    .then (data => { data.forEach(workout => showWorkouts(workout))
-       
-       
+    .then (data => { 
+        data.forEach(workout => showWorkouts(workout))
     })
 }
 addWorkouts()
@@ -26,3 +32,19 @@ function showWorkouts(workout){
     div.append(h2, img, h3)
     workoutCollection.append(div)
 }
+
+// function callBackFunction(e){
+//     console.log(e, "it works")
+//     submit.preventDefault()
+//     submit.addEventListener("click", event => callBackFunction(event));
+// }
+
+document.addEventListener("change", event =>{
+    if(event.target.matches("#muscle-group")){
+        const ul = document.getElementById("create_workout")
+        ul.innerHTML = ""
+        const filterWorkout = workouts.filter(workout => workout.target[0] === event.target.value)
+ 
+        addWorkouts(filterWorkout)
+    }
+    })
