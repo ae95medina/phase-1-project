03a7aa6596d3825d751f5a93c244099d81e5ed49
ae95medina <ae95medina@gmail.com>
2022-06-21 09:27:20 -0500

    first filter

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,23 +1,16 @@
-console.log("hi")
-document.addEventListener('DOMContentLoaded', () => {
-document.querySelector('form').addEventListener('submit', e => {
-    e.preventDefault()
-})
-
-})
+let workouts=[];
 
 function addWorkouts(){
-// const createWorkout = document.querySelector("submit")
-// createWorkout.addEventListener("submit", event){
-    
-// }
 fetch("http://localhost:3000/workouts")
     .then (response => response.json())
     .then (data => { 
-        data.forEach(workout => showWorkouts(workout))
+        workouts= data;
+        workouts.forEach(workout => showWorkouts(workout))
+        
     })
 }
 addWorkouts()
+
 function showWorkouts(workout){
     const workoutCollection = document.getElementById("workout_collection")
     const div = document.createElement("div")
@@ -28,23 +21,21 @@ function showWorkouts(workout){
     img.src = workout.image
     img.classList.add("exercise-image")
     const h3 = document.createElement("h3")
-    h3.textContent = workout.target
+     h3.textContent = workout.target
     div.append(h2, img, h3)
     workoutCollection.append(div)
 }
 
-// function callBackFunction(e){
-//     console.log(e, "it works")
-//     submit.preventDefault()
-//     submit.addEventListener("click", event => callBackFunction(event));
-// }
-
 document.addEventListener("change", event =>{
-    if(event.target.matches("#muscle-group")){
-        const ul = document.getElementById("create_workout")
-        ul.innerHTML = ""
-        const filterWorkout = workouts.filter(workout => workout.target[0] === event.target.value)
- 
-        addWorkouts(filterWorkout)
+    if(event.target.matches("#primary")){
+        console.log(event.target.value)
+        const filterWorkouts = workouts.filter(workout => workout.target === event.target.value)
+       
+        document.getElementById("workout_collection").innerHTML= "";
+       
+        filterWorkouts.forEach(workout=> {
+        showWorkouts(workout)
+       }) 
     }
     })
+    
